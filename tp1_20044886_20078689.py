59bceecc494d2ca1ad382be8d1c77eeb5cb4cac1
Andre <derkan.man@gmail.com>
2017-03-20 10:49:49 -0400

    commentaires et docstring

--- tp1_20044886_20078689.py
+++ tp1_20044886_20078689.py
@@ -3,20 +3,28 @@ import random
 
 
 class Game:
+    """
+    Classe pour une sous-partie
+    """
     def __init__(self, jeu):
         self._jeu = jeu
 
     def winner(self):
-        # 0 partie non terminee
-        # 1 x gagnant
-        # 2 o gagnant
-        # 3 partie nulle
+        """
+        Retourne le gagnant d'une sous-partie
+        si non terminée: 0
+        si x gagne: 1
+        si o gagne: 2
+        si nulle: 3
+        :return: état de la partie en int
+        """
+        # Liste des possibilités de gagner.
         winners = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                    [0, 3, 6], [1, 4, 7], [2, 5, 8],
                    [0, 4, 8], [2, 4, 6]]
         win = 0
         cases_vides = 0
-        nulle = 3  # code pour partie nulle
+        nulle = 3  # Code pour partie nulle.
 
         for i in range(0, 9):
             valeur = self._jeu >> ((8 - i) << 1) & 3
@@ -40,17 +48,21 @@ class Game:
                     return win
 
         return nulle if (cases_vides == 0 and win == 0) else win
-##End Game Class
+# End Game Class
+
 
 class MetaGame:
+    """
+    Classe pour stocker un jeu de UTT
+    """
     def __init__(self, entier):
         self._entier = entier
         self._last = entier >> 162 & 127
         value = entier >> ((80 - self._last) << 1) & 3
         if entier == 0:
-            self._player = 1  # premier coup de la partie, joueur = x
+            self._player = 1  # Premier coup de la partie, joueur = x.
         else:
-            self._player = value ^ 0b11  # XOR avec 0b11 pour inverser le player
+            self._player = value ^ 0b11  # Obtenir le joueur courant.
 
     def get_entier(self):
         return self._entier
@@ -65,31 +77,47 @@ class MetaGame:
         return str(self._entier)
 
     def winner(self):
-        meta_tictactoe = 1  # UTT sous forme d'un tictactoe 3x3
+        """
+        Crée une sous-partie abstraite pour tester si la partie de UTT est terminée
+        si non-terminée: 0
+        si x gagne: 1
+        si o gagne: 2
+        si nulle : 3
+        :return: état de la partie en int
+        """
+        meta_tictactoe = 1  # UTT sous forme d'un tictactoe 3x3.
         for i in range(0, 9):
-            r = range(0 + i * 9, 9 + i * 9)  # extraction d'une sous-partie
-            tmp_game = 1  # entier pour stocker une sous-partie
+            r = range(0 + i * 9, 9 + i * 9)  # Extraction d'une sous-partie.
+            tmp_game = 1  # Entier pour stocker une sous-partie.
             for j in r:
                 value = self._entier >> ((80 - j) << 1) & 3
                 tmp_game = (tmp_game << 2) + value
             tmpgame = Game(tmp_game)
-            value = tmpgame.winner()  # test chaque sous-partie pour un gagnant
+            value = tmpgame.winner()  # Test chaque sous-partie pour un gagnant.
             value = 0 if value == 3 else value
             meta_tictactoe = (meta_tictactoe << 2) + value
 
-        # meta_tictactoe devient un obj Game, test pour winner
         tmpgame = Game(meta_tictactoe)
         return tmpgame.winner()
 
     def getInt(self, move):
+        """
+        Retourne une nouvelle représentation d'une partie suite à un coup joué
+        :param move: Position où le coup est joué
+        :return: Partie sous forme d'un int
+        """
         new_int = (self._player << ((80 - move) << 1)) + self._entier
-        new_int &= ~(127 << 162)  # Supprime dernier bit jouer ( 7 1er bits )
+        new_int &= ~(127 << 162)  # Supprime dernier bit jouer ( 7 1er bits ).
         new_int += (move << 162)
         return new_int
 
     def possibleMoves(self):
+        """
+        Retourne les coups possibles pour le joueur courant
+        :return: Liste de int représentant les indices
+        """
         possible = []
-        if self.winner() != 0:  # la partie est deja gagnée
+        if self.winner() != 0:  # La partie est deja gagnée.
             return possible
 
         if self._entier == 0:
@@ -98,13 +126,13 @@ class MetaGame:
         next_case = self._last % 9
         indice_deb = next_case * 9
         range_indice = range(indice_deb, indice_deb + 9)
-        tmp_game = 1  # entier pour stocker une sous-partie
+        tmp_game = 1  # Entier pour stocker une sous-partie.
 
         for i in range_indice:
             value = self._entier >> ((80 - i) << 1) & 3
-            tmp_game = (tmp_game << 2) + value  # construire un petit tic tac toe
+            tmp_game = (tmp_game << 2) + value  # Construire une sous-partie.
             if value == 0:
-                possible.append(i)  # garder les coups possibles en memoire
+                possible.append(i)  # Garder les coups possibles en mémoire.
 
         tmpgame = Game(tmp_game)
         if tmpgame.winner() != 0:
@@ -112,7 +140,7 @@ class MetaGame:
 
         if not possible:  # Il faut jouer dans une autre sous-partie.
             for i in range(0, 9):
-                if i == next_case:  # Skip la case analyser precedemment
+                if i == next_case:  # Skip la case analyser precedemment.
                     continue
                 r = range(0 + i * 9, 9 + i * 9)
                 tmp_game = 1
@@ -125,13 +153,16 @@ class MetaGame:
                         tmp_possible.append(j)
 
                 tmpgame = Game(tmp_game)
-                winstate = tmpgame.winner()  # test chaque sous partie pour un gagnant
-                if winstate == 0:  # la partie n'est pas fini, on peut y jouer
+                winstate = tmpgame.winner()  # Test chaque sous partie pour un gagnant.
+                if winstate == 0:  # La sous-partie n'est pas fini, on peut y jouer.
                     possible.extend(tmp_possible)
 
         return possible
 
     def OutputBoard(self):
+        """
+        Imprime la partie pour la lecture humaine
+        """
         indicator = 0
         line = ""
         signe = [" . ", " x ", " o ", " ? "]
@@ -141,16 +172,16 @@ class MetaGame:
                 for i in range(0, 3):
 
                     values = []
-                    ## valeur des 3 cases d'une ligne d'une petite partie
+                    # Valeur des 3 cases d'une ligne d'une petite partie.
                     for ii in range(0,3):
                         value = self._entier >> ((80 - ((indicator + ii) + i * 9)) << 1) & 3
                         values.append(value)
 
                     slot = indicator + i * 9
 
-                    ##ajoute les 3 cases à notre ligne d'affichage
+                    # Ajoute les 3 cases à notre ligne d'affichage.
                     for ii in range(0, 3):
-                        ## vérifie si on affiche le dernier coup jouer
+                        # Vérifie si on affiche le dernier coup jouer.
                         if slot + ii != self._last:
                             line += signe[values[ii]]
                         else:
@@ -164,9 +195,13 @@ class MetaGame:
             if k != 2:
                 print("-" * 29)
             indicator += 18
-##End MetaGame Class
+# End MetaGame Class
+
 
 class Node:
+    """
+    Classe pour la gestion d'un noeud d'un arbre
+    """
     def __init__(self, data):
         self._data = data
         self._children = []
@@ -181,21 +216,21 @@ class Node:
         return self._children
 
     def sample(self, n):
-        # faire les stats sur le dernier coup joué
-        # ex: quels sont les chances de win si on joue sur la case 54
-        # le prochain coup est a faire par l'adversaire
-        # last = self._data.get_last()
-
-        # structure pour stat [0] nombre total de simulation [1] win de x [2] win de o
-
+        """
+        Teste un nombre n de partie aléatoire pour générer des statistiques sur les coups possibles
+        Une statistique est stockée ainsi:
+            [nombre de parties jouées, gagné par x, gagné par o]
+        :param n: nombre de partie à jouer pour chaque coup
+        :return: Liste de statistiques pour chaque coup
+        """
         stats_win = [n, 0, 0]
-        possible = self._data.possibleMoves()  # get les coups possibles de l'adversaire
+        possible = self._data.possibleMoves()  # Obtenir les coups possibles de l'adversaire.
 
         if not possible:
             return 0
 
-        for i in range(0, n):  # faire n simulation, chaque simulation se fait jusqu'a la fin de la partie
-            tmpmeta = self._data
+        for i in range(0, n):  # Faire n simulation, chaque simulation se fait jusqu'a la fin de la partie.
+            tmpmeta = self._data  # Réinitialiser la partie temporaire.
             fin_de_partie = False
 
             while not fin_de_partie:
@@ -205,36 +240,39 @@ class Node:
                 if not possible:
                     break
 
-                choix_random = random.choice(possible)  # prendre un coup au hasard
+                choix_random = random.choice(possible)  # Prendre un coup au hasard.
 
-                tmpmeta = MetaGame(tmpmeta.getInt(choix_random))  # faire le coup
+                tmpmeta = MetaGame(tmpmeta.getInt(choix_random))  # Faire le coup.
 
-                # test fin de partie et update stats
+                # Teste pour une fin de partie et mise à jour des stats.
                 win = tmpmeta.winner()
 
                 if win == player:
                     stats_win[player] += 1
                     break
-                elif win == 3:  # partie est nulle, pas de stats
+                elif win == 3:  # La partie est nulle, pas de stats.
                     break
 
         return stats_win
 
     def __str__(self):
         return str(self.get_data())
-##End Node Class
+# End Node Class
 
 
-<<<<<<< HEAD
-=======
-
-##Function for p parameter
->>>>>>> d3f6953d0bf89bc07a6063fd0c965dc80db12aef
 def p_mode(entier):
+    """
+    Gère le paramètre p, imprime la partie pour la lecture
+    :param entier: partie de UTT sous forme de int
+    """
     MetaGame(entier).OutputBoard()
 
-##Function for no parameter
+
 def no_mode(entier):
+    """
+    Choisis le prochain coup à jouer pour gagner
+    :param entier: Partie de UTT sous forme de int
+    """
     main_node = Node(MetaGame(entier))
     stats = []
     ratio = []
@@ -244,35 +282,28 @@ def no_mode(entier):
         game_possible = MetaGame(main_node.get_data().getInt(coup))
         main_node.add_child(Node(game_possible))
 
-    # test chaque enfant pour une fin de partie
-    for child in main_node.get_children():
-        # print(child)
-        if child.get_data().winner() == main_node.get_data().get_player():  # si le winner est le parent, win
+    for child in main_node.get_children():  # Test chaque enfant pour une fin de partie.
+        if child.get_data().winner() == main_node.get_data().get_player():  # Le joueur courant gagne.
             print(child.get_data().get_entier())
-            # print(bin(child.get_data().get_entier()))
-            # print(child.get_data().get_last())
-            # child.get_data().OutputBoard()
-            sys.exit(0)
+            sys.exit(0)  # On a trouvé le coup à jouer
 
-    # aucun enfant ne termine la partie, constuire les stats
-
-
-    for child in main_node.get_children():
+    for child in main_node.get_children():  # Construire les stats.
         stats.append(child.sample(1000))
 
-    print(stats)
-    # choisir meilleure stat
     for i in range(0, len(stats)):
-        ratio.append(stats[i][main_node.get_data().get_player()] / stats[i][0])  # nombre de partie/total
+        ratio.append(stats[i][main_node.get_data().get_player()] / stats[i][0])  # Nombre de partie/total.
 
-    # get le best coup a jouer
-    best_coup = main_node.get_children()[ratio.index(max(ratio))]
+    best_coup = main_node.get_children()[ratio.index(max(ratio))]  # Choisir le meilleur coup.
     print(best_coup.get_data())
-##end no_mode Function
 
 
-##Function for a parameter
 def a_mode(profondeur, entier):
+    """
+    Gère le paramètre a.
+    Imprime les coups possibles selon la profondeur
+    :param profondeur: Profondeur de l'arbre à imprimer
+    :param entier: Partie de UTT
+    """
     rootgame = MetaGame(entier)
     child = [rootgame]
     next_child = []
@@ -305,7 +336,7 @@ else:
     entier = int(sys.argv[1])
     no_mode(entier)
 
-# entier = 459329034283597291728327479273734123385595894269204
+# 459329034283597291728327479273734123385595894269204
 # 459329034283597291728327479273734123420780266358036 exemple du tp
 # 457867532646266388810123794441017840401124333815060 modifié le o en pos 0 pour un x
 # 459329034283597291728327479273734123385595894269204 mod 0 en pos 58 en .
